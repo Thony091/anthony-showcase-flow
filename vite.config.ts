1da23c5d828595a32b19b6943c74fb983f963248
Thony091 <121407856+Thony091@users.noreply.github.com>
2025-07-29 22:25:10 -0400

    Actualizar base path para nuevo nombre de repositorio

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/thony-portfolio./', 
+  base: '/thony-portfolio/',
   server: {
     host: "::",
     port: 8080,
